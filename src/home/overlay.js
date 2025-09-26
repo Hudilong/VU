9cb3ae16702f3437bdcaa9e0de289b6c6e85c4b3
Ian Hudicourt <ihudicourt@gmail.com>
2025-09-25 22:23:00 -0400

    test for overlay

--- src/home/overlay.js
+++ src/home/overlay.js
@@ -1,3 +1,11 @@
 (() => {
-
+    const overlay = document.querySelector("#Q1957128348");
+    if (sessionStorage.getItem("overlayDismissed") === "true") {
+        overlay.style.display = "none";
+        return;
+    }
+    
+    overlay.addEventListener("click", () => {
+        sessionStorage.setItem("overlayDismissed", "true");
+    });
 })();
