41ddda6c56c93e536a3d9bfc21ec001833547b61
Ian Hudicourt <ihudicourt@gmail.com>
2025-09-25 22:24:22 -0400

    added logs.

--- src/home/overlay.js
+++ src/home/overlay.js
@@ -1,5 +1,6 @@
 (() => {
     const overlay = document.querySelector("#Q1957128348");
+    console.log(overlay);
     if (sessionStorage.getItem("overlayDismissed") === "true") {
         overlay.style.display = "none";
         return;
@@ -7,5 +8,6 @@
     
     overlay.addEventListener("click", () => {
         sessionStorage.setItem("overlayDismissed", "true");
+        console.log('overlay was clicked');
     });
 })();
