987fbc714a9aa73cabf184cf3d3958d0d3867ac4
Ian Hudicourt <ihudicourt@gmail.com>
2025-10-04 22:26:07 -0400

    ..

--- src/home/overlay.js
+++ src/home/overlay.js
@@ -1,20 +1,22 @@
 (() => {
-    const overlayElements = [document.querySelector("#Q1957128348"), document.querySelector("#R1668069488")];
-    console.log(overlayElements);
+    const overlay = document.querySelector("#Q1957128348");
+    console.log(overlay);
     if (sessionStorage.getItem("overlayDismissed") === "true") {
         hideOverlay();
         return;
     } else {
-        overlayElements.forEach(element => element.addEventListener("click", () => {
+        overlay.addEventListener("click", () => {
             sessionStorage.setItem("overlayDismissed", "true");
             console.log('overlay was clicked');
-        }));
+        });
     }
 
     function hideOverlay() {
         const closeOverlayLink = document.querySelector('[rel="close-overlay"]');
         if (closeOverlayLink) {
             closeOverlayLink.click();
+        } else {
+            overlay.style.display = "none";
         }
     }
 })();
